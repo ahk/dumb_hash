a92a53f2bee52760f5ab6bff1ddc7cefffe64087
Andrew Hay Kurtz <andrew.hay.kurtz@gmail.com>
2010-02-03 04:35:33 -0800

    test for collisions in benchmark

--- benchmarks/bencher.rb
+++ benchmarks/bencher.rb
@@ -2,13 +2,18 @@ require 'benchmark'
 require File.dirname(__FILE__) + '/../dumb_hash'
 
 DumbHash::IMPLEMENTATIONS.each do |implementation|
-  [1000_000].each do |n|
+  [10, 100, 1000, 10000].each do |n|
     puts "#{n} times DOING:"
     hash = implementation.new
     puts implementation.to_s
     Benchmark.bm do |x|
-      x.report('set') {n.times { |i| hash.set("key#{i}".to_sym, "val#{i}".to_sym) }}
-      x.report('get') {n.times { |i| hash.get("key#{i}".to_sym) }}
+      x.report('set') {n.times { |i| hash.set(:"key#{i}", :"val#{i}") }}
+      x.report('get') do 
+        n.times do |i|
+          val = hash.get(:"key#{i}")
+          raise "Collision, k:key#{i} v:#{val}" if val  != :"val#{i}"
+        end
+      end
     end
   end
 end
